9ab46468f014ce0f53c3363802c46fa443150f5d
jessicat8 <jessicatong14@gmail.com>
2025-03-22 18:07:52 -0700

    routing from home to read

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      HomePage
+      <Link to={`/read`}>Read</Link>
+    </div>
+  );
 };
 
 export default HomePage;
