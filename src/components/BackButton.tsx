26d90e58a86281a81eb32c7e7bb97d9f1e4894a1
jessicat8 <jessicatong14@gmail.com>
2025-03-27 00:43:50 -0700

    change to chevron

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,15 @@
-import { FaArrowLeft } from "react-icons/fa6";
+import { FaChevronLeft } from "react-icons/fa6";
 import { useReadContext } from "./ReadContext";
 
 const BackButton = () => {
   const { handleBack, backIsActive } = useReadContext();
 
   return (
-    <button
-      className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full text-white hide-portrait  
-      ${backIsActive ? "bg-sky-400  hover:bg-sky-500 transition" : "bg-gray-400"}`}
-      onClick={handleBack}
-    >
-      <FaArrowLeft className="text-lg" />
+    <button onClick={handleBack}>
+      <FaChevronLeft
+        className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full hide-portrait  
+        ${backIsActive ? "text-sky-300  hover:text-sky-500 transition cursor-pointer" : "text-gray-400"}`}
+      />
     </button>
   );
 };
--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -1,15 +1,14 @@
-import { FaArrowRight } from "react-icons/fa6";
+import { FaChevronRight } from "react-icons/fa6";
 import { useReadContext } from "./ReadContext";
 
 const NextButton = () => {
   const { handleNext, nextIsActive } = useReadContext();
   return (
-    <button
-      className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full text-white hide-portrait  
-        ${nextIsActive ? "bg-sky-400  hover:bg-sky-500 transition" : "bg-gray-400"}`}
-      onClick={handleNext}
-    >
-      <FaArrowRight className="text-lg" />
+    <button onClick={handleNext}>
+      <FaChevronRight
+        className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full hide-portrait  
+        ${nextIsActive ? "text-sky-300  hover:text-sky-500 transition cursor-pointer" : "text-gray-400"}`}
+      />
     </button>
   );
 };
