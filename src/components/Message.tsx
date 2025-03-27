677607e4b14a8e79374e6a4feb758ace6b3bb638
jessicat8 <jessicatong14@gmail.com>
2025-03-27 01:20:07 -0700

    delete practice files

--- src/components/Message.tsx
+++
@@ -1,9 +0,0 @@
-// function Message() {
-//     return <h1>Hello World</h1>;
-// }
-
-const Message = () => {
-  const name = "Jessica";
-  return <h1>Hello {name ? name : "World"}</h1>;
-};
-export default Message;
