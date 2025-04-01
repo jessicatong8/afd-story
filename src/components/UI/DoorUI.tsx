2361133f238b42cd36d4f55fb2858433f8e5f66d
jessicat8 <jessicatong14@gmail.com>
2025-04-01 00:03:14 -0700

    update color

--- src/components/UI/DoorUI.tsx
+++ src/components/UI/DoorUI.tsx
@@ -29,8 +29,8 @@ const DoorUI = () => {
         <g filter="url(#filter0_f_1731_717)">
           <path
             d="M1 21.5C1 21.5 227 6.49998 232.5 10.5C238 14.5 235.231 50.5 236.5 64.5C237.769 78.5 244.5 106 247.5 146C250.5 186 249.5 261.5 251.5 298C252.283 312.294 251.679 313 255.339 372.5C259 432 259.913 470.979 259 472.5C257.5 475 1 473 1 473"
-            stroke="#FFE600"
-            stroke-opacity="0.5"
+            stroke="#FFDD53"
+            stroke-opacity="0.7"
             stroke-width={strokeWidth}
             stroke-linecap="round"
             stroke-linejoin="round"
