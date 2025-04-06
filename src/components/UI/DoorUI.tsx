8dae5581ab776ac69551ed1adaf85f69e3a2eab2
jessicat8 <jessicatong14@gmail.com>
2025-04-05 19:59:57 -0700

    update glow

--- src/components/UI/DoorUI.tsx
+++ src/components/UI/DoorUI.tsx
@@ -16,9 +16,10 @@ const DoorUI = () => {
   return (
     <button onClick={onClick}>
       <svg
-        className="absolute top-[29%] left-[13.5%] -translate-x-[50%]
-       -translate-y-[50%] w-1/2 h-auto scale-55 cursor-pointer"
+        className="absolute top-[29%] left-[14%] -translate-x-[50%]
+       -translate-y-[50%] w-1/4 h-auto scale-112 cursor-pointer"
         onMouseEnter={() => setStrokeWidth("20")}
+        onTouchStart={() => setStrokeWidth("20")}
         onMouseLeave={() => setStrokeWidth("10")}
         width="269"
         height="483"
@@ -29,7 +30,7 @@ const DoorUI = () => {
         <g filter="url(#filter0_f_1731_717)">
           <path
             d="M1 21.5C1 21.5 227 6.49998 232.5 10.5C238 14.5 235.231 50.5 236.5 64.5C237.769 78.5 244.5 106 247.5 146C250.5 186 249.5 261.5 251.5 298C252.283 312.294 251.679 313 255.339 372.5C259 432 259.913 470.979 259 472.5C257.5 475 1 473 1 473"
-            stroke="#FFDD53"
+            stroke="#FFEC5B"
             stroke-opacity="0.7"
             stroke-width={strokeWidth}
             stroke-linecap="round"
