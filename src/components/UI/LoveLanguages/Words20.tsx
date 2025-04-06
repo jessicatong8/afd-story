f62c01adad90111d8ee0021bbb3a20d1a7c19cc7
jessicat8 <jessicatong14@gmail.com>
2025-04-06 16:02:25 -0700

    update glow shape

--- src/components/UI/LoveLanguages/Words20.tsx
+++ src/components/UI/LoveLanguages/Words20.tsx
@@ -8,18 +8,18 @@ const Words20 = ({ clicked, hover }: Props) => {
   return (
     <span>
       <svg
-        className={`absolute w-1/2 h-1/2 scale-116 top-[62.2%] left-[36.2%] -translate-x-[50%] -translate-y-[50%] transition-opacity duration-1000
+        className={`absolute w-1/2 h-1/2 scale-114 top-[62.2%] left-[36.2%] -translate-x-[50%] -translate-y-[50%] transition-opacity duration-1000
         ${clicked ? "opacity-0" : "opacity-100"}
         `}
-        width="556"
-        height="348"
-        viewBox="0 0 556 348"
+        width="569"
+        height="360"
+        viewBox="0 0 569 360"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g filter="url(#filter0_f_2217_1080)">
           <path
-            d="M379 31.1113C348.475 23.8512 314.189 18.8764 276.632 16.2996C114.635 5.18473 20.3878 92.3825 16.0792 180.3C11.7706 268.217 85.118 331.278 281.471 332.757C477.825 334.236 541.798 265.748 540.779 174.445C540.083 112.061 492.648 66.2845 410.995 40"
+            d="M389.315 27.4282C357.468 19.6661 321.699 14.3436 282.518 11.5811C113.518 -0.33421 15.222 92.7662 10.7515 186.66C6.28104 280.554 82.8156 347.923 287.655 349.557C492.494 351.19 559.212 278.063 558.124 180.552C557.381 113.926 507.884 65.0244 422.694 36.9302"
             stroke="#FFEC5B"
             stroke-opacity="0.7"
             stroke-width={clicked || hover ? "20" : "10"}
@@ -28,10 +28,10 @@ const Words20 = ({ clicked, hover }: Props) => {
         <defs>
           <filter
             id="filter0_f_2217_1080"
-            x="0.900879"
-            y="0.341797"
-            width="554.89"
-            height="347.439"
+            x="0.567871"
+            y="0.550781"
+            width="567.569"
+            height="359.033"
             filterUnits="userSpaceOnUse"
             color-interpolation-filters="sRGB"
           >
