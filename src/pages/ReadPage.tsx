201d9e208fc14a5ebd81032134a508ebd09bcc31
jessicat8 <jessicatong14@gmail.com>
2025-04-05 20:00:19 -0700

    disable scroll

--- src/pages/ReadPage.tsx
+++ src/pages/ReadPage.tsx
@@ -5,8 +5,18 @@ import BackButton from "../components/BackButton";
 import NextButton from "../components/NextButton";
 import NavigationBar from "../components/NavigationBar";
 import ProgressBar from "../components/ProgressBar";
+import { useEffect } from "react";
 
 function ReadPage() {
+  useEffect(() => {
+    // Disable scrolling when component mounts
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      // Re-enable scrolling when component unmounts
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
   return (
     <div>
       {/* <NavigationBar /> */}
