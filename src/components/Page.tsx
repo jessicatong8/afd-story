5fa07cc37abfd848da58a40bbd094844a258df93
jessicat8 <jessicatong14@gmail.com>
2025-04-09 21:55:39 -0700

    cleaner preloading of pages and UI

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -7,12 +7,6 @@ import { useSwipeable } from "react-swipeable";
 import { Navigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
-// import DoorUI from "./UI/DoorUI";
-// import DumplingFillingUI from "./UI/DumplingFilling/DumplingFillingUI";
-// import DumplingGivingUI from "./UI/DumplingGiving/DumplingGivingUI";
-// import ScratchReveal from "./UI/ThoughtReveal/ScratchReveal";
-// import LoveLanguagesUI from "./UI/LoveLanguages/LoveLanguagesUI";
-
 // import UI elements lazily
 const DoorUIImport = () => import("./UI/DoorUI");
 const DoorUI = lazy(DoorUIImport);
@@ -28,9 +22,8 @@ const LoveLanguagesUIImport = () =>
   import("./UI/LoveLanguages/LoveLanguagesUI");
 const LoveLanguagesUI = lazy(LoveLanguagesUIImport);
 
-// Import all images dynamically from the assets folder
+// Import all page images dynamically from the assets folder
 const images = import.meta.glob("/src/assets/pages/*.png");
-// console.log(images);
 
 const Page = () => {
   const {
@@ -55,83 +48,76 @@ const Page = () => {
     return <Navigate to="/not-found" replace />; //replace to avoid the browser back button leading back to an invalid page
   }
 
-  useEffect(() => {
-    // there's probably a better way to do this with a dictionary, would be nice to preload components in a range like the pages
-    // setImageLoaded(false);
-    // disable navigation to the next page depending on the current page to facilitate UI
-    toggleNext(
-      currentPage !== 3 &&
-        currentPage !== 5 &&
-        currentPage !== 15 &&
-        currentPage !== 17
-    );
-    // preload UI components 2 pages before rendering
-    currentPage === 1 && DoorUIImport();
-    currentPage === 3 && ScratchRevealImport();
-    currentPage === 13 && DumplingFillingUIImport();
-    currentPage === 15 && DumplingGivingUIImport();
-    currentPage === 15 && LoveLanguagesUIImport();
-  }, [currentPage]);
-
-  // Function to load an image dynamically
-  const loadImage = async (page: number) => {
-    const path = `/src/assets/pages/pg_${page}.png`;
-
-    // console.log(`Trying to load image for page: ${page}`); // Debugging: check page number
-    // console.log(`Image path: ${path}`); // Debugging: check the generated path
-
-    if (images[path]) {
-      // console.log(`Found image for page: ${page}`); // Debugging: found image
-      const imageModule = (await images[path]()) as { default: string };
-      // console.log(`Image URL: ${imageModule.default}`); // Debugging: check the image URL
-      return imageModule.default;
-    }
-    console.log(`No image found for page: ${page}`); // Debugging: image not found
-    return "";
-  };
-
-  useEffect(() => {
-    const loadImages = async () => {
-      const newCache: Record<number, string> = { ...imageCache };
-
-      const pagesToLoad = [
-        currentPage - 2,
-        currentPage - 1,
-        currentPage,
-        currentPage + 1,
-        currentPage + 2,
-      ];
-
-      for (const page of pagesToLoad) {
-        if (page >= 0 && page <= numPages && !newCache[page]) {
-          newCache[page] = await loadImage(page);
-        }
-      }
-
-      setImageCache(newCache);
-    };
-
-    loadImages();
-  }, [currentPage]);
   // handle swipe gestures, swipe left or right to control page navigation
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => handleNext(),
     onSwipedRight: () => handleBack(),
   });
 
-  // handle key presses, use left and right arrow keys to control page navigation
+  // handle keyboard navigation, use left and right arrow keys to control page navigation
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "ArrowLeft") {
         handleBack();
-      } else if (event.key === "ArrowRight") {
+      }
+      if (event.key === "ArrowRight") {
         handleNext();
       }
     };
-
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [currentPage, nextIsActive]);
+
+  // Preload page content
+
+  // Dynamically load page images
+  const loadImage = async (page: number) => {
+    const path = `/src/assets/pages/pg_${page}.png`;
+    if (images[path]) {
+      const imageModule = (await images[path]()) as { default: string };
+      return imageModule.default;
+    }
+    console.log(`No image found for page: ${page}`); // Debugging: image not found
+    return "";
+  };
+  // imports for UI components on each page
+  const loadUIComponents: Record<number, () => Promise<any>> = {
+    3: DoorUIImport,
+    5: ScratchRevealImport,
+    15: DumplingFillingUIImport,
+    17: DumplingGivingUIImport,
+  };
+
+  // Preload 3 pages back and forth around current page
+  useEffect(() => {
+    const preload = async () => {
+      // array of the 7 pages around current page
+      const pagesToLoad = Array.from(
+        { length: 7 },
+        (_, i) => currentPage - 3 + i
+      );
+      const newCache: Record<number, string> = { ...imageCache };
+
+      for (const page of pagesToLoad) {
+        // load page images
+        if (page >= 0 && page <= numPages && !newCache[page]) {
+          newCache[page] = await loadImage(page);
+        }
+        // load UI components
+        if (loadUIComponents[page]) {
+          loadUIComponents[page]();
+        }
+      }
+      setImageCache(newCache);
+    };
+
+    preload();
+  }, [currentPage]);
+
+  // Disable navigation to next page for UI pages
+  useEffect(() => {
+    toggleNext(![3, 5, 15, 17].includes(currentPage));
+  }, [currentPage]);
 
   const animationVariants = {
     enter: (direction: number) => ({
@@ -153,47 +139,26 @@ const Page = () => {
       {...swipeHandlers}
       className="w-full h-screen flex justify-center items-center"
     >
-      <AnimatePresence custom={direction} initial={false}>
+      {imageCache[currentPage] ? (
         <div className="relative aspect-square h-screen square-portrait border-2 border-sky-200 overflow-clip">
-          {imageCache[currentPage] ? (
-            <motion.div
-              className="absolute"
-              custom={direction}
-              variants={animationVariants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                // type: "spring",
-                // stiffness: 500,
-                // damping: 30,
-                // mass: 0.2,
-                duration: 0.4,
-              }}
-              key={currentPage}
-            >
-              <LazyLoadImage
-                src={imageCache[currentPage]}
-                alt={`Page ${currentPage}`}
-                // effect="blur"
-                // onLoad={() => setImageLoaded(true)}
-                className="object-contain"
-              />
-              {currentPage === 3 && <DoorUI />}
-              {currentPage === 5 && <ScratchReveal />}
-              {currentPage === 15 && <DumplingFillingUI />}
-              {currentPage === 17 && <DumplingGivingUI />}
-              {currentPage === 20 && <LoveLanguagesUI />}
-              {currentPage === 28 && <LoveLanguagesUI />}
-              {currentPage === 29 && <LoveLanguagesUI />}
-              {currentPage === 30 && <LoveLanguagesUI />}
-              {currentPage === 31 && <LoveLanguagesUI />}
-            </motion.div>
-          ) : (
-            <p>Loading...</p>
-          )}
+          <img
+            src={imageCache[currentPage]}
+            alt={`Page ${currentPage}`}
+            className="object-contain"
+          />
+          {currentPage === 3 && <DoorUI />}
+          {currentPage === 5 && <ScratchReveal />}
+          {currentPage === 15 && <DumplingFillingUI />}
+          {currentPage === 17 && <DumplingGivingUI />}
+          {currentPage === 20 && <LoveLanguagesUI />}
+          {currentPage === 28 && <LoveLanguagesUI />}
+          {currentPage === 29 && <LoveLanguagesUI />}
+          {currentPage === 30 && <LoveLanguagesUI />}
+          {currentPage === 31 && <LoveLanguagesUI />}
         </div>
-      </AnimatePresence>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
