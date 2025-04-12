a82e88499bfedf40389bb8e19c0dd668aceb6eff
jessicat8 <jessicatong14@gmail.com>
2025-04-12 00:28:40 -0700

    hide on first page

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,13 +2,15 @@ import { FaChevronLeft } from "react-icons/fa6";
 import { useReadContext } from "./ReadContext";
 
 const BackButton = () => {
-  const { handleBack, backIsActive } = useReadContext();
+  const { handleBack, backIsActive, currentPage } = useReadContext();
 
   return (
     <button onClick={handleBack}>
       <FaChevronLeft
-        className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full hide-portrait  
-        ${backIsActive ? "text-sky-300  hover:text-sky-500  active:text-sky-500 transition cursor-pointer" : "text-gray-300"}`}
+        className={`hidden lg:flex w-12 h-12 items-center justify-center rounded-full hide-portrait transition
+        ${currentPage === 0 && "opacity-0 "}
+        ${backIsActive ? "text-sky-300  hover:text-sky-500 active:text-sky-500  cursor-pointer" : "text-gray-300"}
+      `}
       />
     </button>
   );
