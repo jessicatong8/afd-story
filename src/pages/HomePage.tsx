bf6fc99cc6af99a2fd6550c6f9ca7aa6abea2d0e
jessicat8 <jessicatong14@gmail.com>
2025-04-12 02:22:55 -0700

    start home and game page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import cover from "/cover.png";
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-2">
-      HomePage
-      <Link to={`/read`}>Read</Link>
+      <div className="w-full h-screen object-scale-down">
+        <img src={cover} className="object-scale-down" />
+      </div>
+      <Link to={`/read/0`}>Read</Link>
+      <Link to={`/game`}>Game</Link>
     </div>
   );
 };
