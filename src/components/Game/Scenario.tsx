9b5ebf5172b95749ac13f7f48bb62531c7fc8a5e
jessicat8 <jessicatong14@gmail.com>
2025-04-13 00:40:19 -0700

    initial test

--- src/components/Game/Scenario.tsx
+++ src/components/Game/Scenario.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import image from "../../assets/game/8_emma_mom_sick.png";
 
 const Scenario = () => {
-  return <div>Scenario</div>;
+  return (
+    <div className="w-1/3 h-1/3">
+      <img src={image} />
+    </div>
+  );
 };
 
 export default Scenario;
