7b3fb2c58f9be778c21d9c1aa75d77aef11fd2df
jessicat8 <jessicatong14@gmail.com>
2025-04-14 02:17:00 -0700

    basic home page

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <nav className="w-full flex items-center justify-between px-6 py-4 bg-white shadow-md lg:px-12">
+      <Link to={`/`} className="">
+        <div className="flex flex-row justify-center items-center gap-2">
+          <img src="/favicon.png" className="w-8 h-8"></img>
+          Food is My Love Language
+        </div>
+      </Link>
+      {/* Right: Nav Items */}
+      <div className="flex space-x-6">
+        <Link to={`/read/0`} className="flex justify-center items-center">
+          Read
+        </Link>
+        <Link to={`/game`} className="flex justify-center items-center">
+          Game
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NavigationBar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import cover from "/cover.png";
+import NavigationBar from "../components/NavigationBar";
 const HomePage = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <div className="w-full h-screen object-scale-down">
-        <img src={cover} className="object-scale-down" />
+    <div>
+      <NavigationBar />
+      <div className="flex flex-col justify-center items-center m-6 gap-6">
+        <div className="w-full h-full lg:max-w-1/2">
+          <img src={cover} className="w-full h-auto object-cover shadow-md" />
+        </div>
+        <Link
+          to={`/read/0`}
+          className="flex justify-center items-center rounded px-6 py-6 h-8 transition-all inset-ring-2 shadow-sm inset-ring-blue-secondary bg-blue-primary font-open font-bold 
+          hover:bg-blue-secondary active:scale-95"
+        >
+          Start Reading
+        </Link>
       </div>
-      <Link to={`/read/0`}>Read</Link>
-      <Link to={`/game`}>Game</Link>
     </div>
   );
 };
