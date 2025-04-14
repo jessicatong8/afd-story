f1239e687a1a26b8255b5dfdd0f2043249ced90e
jessicat8 <jessicatong14@gmail.com>
2025-04-14 02:05:13 -0700

    update illustration and transition

--- src/components/Game/LunchBoxOpen.tsx
+++ src/components/Game/LunchBoxOpen.tsx
@@ -23,20 +23,20 @@ const LunchBoxOpen = () => {
   }, []);
 
   return (
-    <AnimatePresence>
-      <motion.div
-        animate={{
-          //   opacity: [0, 1],
-          scale: [0.9, 1, 2],
-          transition: { duration: 3.5, ease: "easeOut", times: [0, 0.9, 1] },
-        }}
-        className={`flex h-screen w-screen justify-center items-center 
+    <motion.div
+      key="open"
+      // initial={{ opacity: 0 }}
+      animate={{
+        // opacity: [1, 1, 1],
+        scale: [0.9, 1, 2],
+        transition: { duration: 3.5, ease: "easeIn", times: [0, 0.9, 1] },
+      }}
+      className={`flex h-screen w-screen scale-58 -translate-y-6 justify-center items-center 
             transition-opacity duration-100 ease-out ${fadeOut ? "opacity-0" : "opacity-100"}`}
-      >
-        <img src={image} className="pointer-events-none" />
-        {}
-      </motion.div>
-    </AnimatePresence>
+    >
+      <img src={image} className="pointer-events-none" />
+      {}
+    </motion.div>
   );
 };
 
--- src/pages/StoryEndPage.tsx
+++ src/pages/StoryEndPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import lunchBoxClosed from "../assets/game/startPage/lunchbox_closed.png";
 import { useEffect, useState } from "react";
 import LunchBoxOpen from "../components/Game/LunchBoxOpen";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const StoryEndPage = () => {
   const navigate = useNavigate();
@@ -17,41 +17,45 @@ const StoryEndPage = () => {
   }, []);
 
   return (
-    <div>
-      {clicked && <LunchBoxOpen />}
-      <div
-        className={`flex flex-col justify-center items-center gap-12 p-6 w-screen h-screen transition-opacity ease-in duration-300 ${clicked && "opacity-0"}`}
-      >
-        <p className="text-center">
-          Do you know all the different ways to express love?
-          <br />
-          Let’s play a game to find out!
-        </p>
-
-        <button
-          onClick={handleGameStart}
-          disabled={clicked === true}
-          className="active:scale-95"
+    <AnimatePresence>
+      {clicked ? (
+        <LunchBoxOpen />
+      ) : (
+        <motion.div
+          key="closed"
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2, ease: "easeOut" }}
+          // transition-opacity ease-in duration-300
+          className={`flex flex-col justify-center items-center gap-12 p-6 w-screen h-screen`}
         >
-          <img src={lunchBoxClosed} className="pointer-events-none" />
-        </button>
+          <p className="text-center">
+            Do you know all the different ways to express love?
+            <br />
+            Let’s play a game to find out!
+          </p>
 
-        <div className="grid grid-cols-2 gap-4 ">
-          <button
-            onClick={() => navigate("/read/1")}
-            className="border-2  border-gray-200 px-4 py-2 rounded "
-          >
-            Read Again
+          <button onClick={handleGameStart} className="active:scale-95">
+            <img src={lunchBoxClosed} className="pointer-events-none" />
           </button>
-          <button
-            onClick={() => navigate("/")}
-            className="border-2  border-gray-200 px-4 py-2 rounded"
-          >
-            Skip
-          </button>
-        </div>
-      </div>
-    </div>
+
+          <div className="grid grid-cols-2 gap-4 ">
+            <button
+              onClick={() => navigate("/read/1")}
+              className="border-2  border-gray-200 px-4 py-2 rounded "
+            >
+              Read Again
+            </button>
+            <button
+              onClick={() => navigate("/")}
+              className="border-2  border-gray-200 px-4 py-2 rounded"
+            >
+              Skip
+            </button>
+          </div>
+        </motion.div>
+      )}
+      {/* {clicked && <LunchBoxOpen />} */}
+    </AnimatePresence>
   );
 };
 
