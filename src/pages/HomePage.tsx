6029f52aafea6e85980c239bef90c9b0cd9af925
jessicat8 <jessicatong14@gmail.com>
2025-04-15 02:14:27 -0700

    update cover

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import cover from "/cover.png";
+import cover from "/cover_button_right.png";
 import NavigationBar from "../components/NavigationBar";
 const HomePage = () => {
   return (
     <div>
       <NavigationBar />
       <div className="flex flex-col justify-center items-center m-6 gap-6">
-        <div className="w-full h-full lg:max-w-1/2">
+        <div className=" relative flex justify-center w-full h-full lg:max-w-1/2">
           <img src={cover} className="w-full h-auto object-cover shadow-md" />
-        </div>
-        <Link
-          to={`/read/0`}
-          className="flex justify-center items-center rounded px-6 py-6 h-8 transition-all inset-ring-2 shadow-sm inset-ring-blue-secondary bg-blue-primary font-open font-bold 
+          <Link
+            to={`/read/0`}
+            className="absolute top-[85%] left-[70%] flex justify-center items-center rounded px-4 py-6 h-1/12 transition-all border-2 border-blue-secondary shadow-md bg-blue-primary font-bold 
           hover:bg-blue-secondary active:scale-95"
-        >
-          Start Reading
-        </Link>
+          >
+            Start Reading
+          </Link>
+        </div>
       </div>
     </div>
   );
