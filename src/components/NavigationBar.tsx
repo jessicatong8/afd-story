6b7602aa4afd51455e0548f7580a587e3d726fb7
jessicat8 <jessicatong14@gmail.com>
2025-04-17 02:58:07 -0700

    styling

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { IoMenuOutline } from "react-icons/io5";
 import { FiMenu } from "react-icons/fi";
 import { useState } from "react";
 import NavigationMenu from "./NavigationMenu";
@@ -7,24 +6,38 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const NavigationBar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const navItemStyle =
+    "flex justify-center px-6 py-6 items-center hover:bg-blue-primary";
 
   return (
     <>
-      <nav className="w-full flex items-center justify-between px-6 py-4 bg-white shadow-md lg:px-12">
+      <nav className="w-full flex items-center justify-between px-6 bg-white shadow-md lg:px-12">
         <Link to={`/`} className="">
-          <div className="flex flex-row justify-center items-center gap-2">
-            <img src="/favicon.png" className="w-8 h-8"></img>
-            <p className="">Food is My Love Language</p>
+          <div className="flex flex-row justify-center items-center gap-3">
+            <img src="/favicon.png" className="w-11 h-11"></img>
+            <div>
+              <p className="font-grand font-semibold mt-0.5">
+                Food is My Love Language
+                <br />
+              </p>
+              <p className="font-open font-medium italic text-sm -mt-1">
+                {" "}
+                An Interactive Story
+              </p>
+            </div>
           </div>
         </Link>
 
         {/* Right: Nav Items */}
-        <div className=" hidden sm:flex space-x-6">
-          <Link to={`/read/0`} className="flex justify-center items-center">
+        <div className=" hidden sm:flex ">
+          <Link to={`/read/0`} className={navItemStyle}>
             Read
           </Link>
-          <Link to={`/game/start`} className="flex justify-center items-center">
+          <Link to={`/game/start`} className={navItemStyle}>
             Game
+          </Link>
+          <Link to={`/`} className={navItemStyle}>
+            About
           </Link>
         </div>
 
