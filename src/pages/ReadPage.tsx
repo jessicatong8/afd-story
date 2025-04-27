0be96d983a25b3a10b5b5d24d3c1bc0652b82717
jessicat8 <jessicatong14@gmail.com>
2025-04-26 23:23:48 -0700

    fade in on start

--- src/pages/ReadPage.tsx
+++ src/pages/ReadPage.tsx
@@ -7,6 +7,7 @@ import ProgressBar from "../components/ProgressBar";
 import { useEffect, useRef, useState } from "react";
 
 import AutoHideNavController from "../components/AutoHideNavClick";
+import { AnimatePresence, motion } from "framer-motion";
 
 function ReadPage() {
   useEffect(() => {
@@ -32,40 +33,46 @@ function ReadPage() {
     if (containerRef.current) {
       observer.observe(containerRef.current);
     }
-
     return () => observer.disconnect();
   }, []);
 
   return (
-    <div className="w-screen h-[100dvh] flex flex-col ">
-      <ReadContextProvider>
-        <AutoHideNavController />
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="w-screen h-[100dvh] flex flex-col "
+      >
+        <ReadContextProvider>
+          <AutoHideNavController />
 
-        <section
-          ref={containerRef}
-          className="flex w-full min-h-0 h-full items-center justify-evenly select-none z-0"
-        >
-          <span className="hide-portrait">
-            <BackButton />
-          </span>
-
-          <div
-            className={`relative  border-blue-tertiary overflow-clip aspect-square shadow-[3px_0_3px_-2px_rgba(0,0,0,0.1),_-3px_0_3px_-2px_rgba(0,0,0,0.1)]
-              ${isLandscape ? "h-full" : "w-full"}`} //set size according to height when parent landscape and width when parent portrait
+          <section
+            ref={containerRef}
+            className="flex w-full min-h-0 h-full items-center justify-evenly select-none z-0"
           >
-            <Page />
+            <span className="hide-portrait">
+              <BackButton />
+            </span>
+
+            <div
+              className={`relative  border-blue-tertiary overflow-clip aspect-square shadow-[3px_0_3px_-2px_rgba(0,0,0,0.1),_-3px_0_3px_-2px_rgba(0,0,0,0.1)]
+              ${isLandscape ? "h-full" : "w-full"}`} //set size according to height when parent landscape and width when parent portrait
+            >
+              <Page />
+            </div>
+
+            <span className=" hide-portrait ">
+              <NextButton />
+            </span>
+          </section>
+
+          <div className="shrink-0">
+            <ProgressBar />
           </div>
-
-          <span className=" hide-portrait ">
-            <NextButton />
-          </span>
-        </section>
-
-        <div className="shrink-0">
-          <ProgressBar />
-        </div>
-      </ReadContextProvider>
-    </div>
+        </ReadContextProvider>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
