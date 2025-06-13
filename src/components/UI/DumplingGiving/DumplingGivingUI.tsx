a1c5659603c4db3c52666bdd900003ee3ddd3c5f
jessicat8 <jessicatong14@gmail.com>
2025-06-13 00:09:08 -0400

    framer motion for pointer, fix nav toggling

--- src/components/UI/DumplingGiving/DumplingGivingUI.tsx
+++ src/components/UI/DumplingGiving/DumplingGivingUI.tsx
@@ -15,9 +15,10 @@ import DumplingHeart from "./DumplingHeart";
 import DumplingDrop from "./DumplingDrop";
 import LoveLanguagesUI from "../LoveLanguages/LoveLanguagesUI";
 import pointerImage from "../../../assets/UI/DumplingGiving/pointer.png";
+import { motion } from "framer-motion";
 
 const DumplingGivingUI = () => {
-  const { toggleBack } = useReadContext();
+  const { toggleBack, toggleNext } = useReadContext();
 
   const [isDropped, setIsDropped] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
@@ -27,6 +28,7 @@ const DumplingGivingUI = () => {
     setIsDragging(false);
     setTimeout(() => {
       toggleBack(true); // delay enabling back so swipe actions don't trigger navigation upon dropping
+      toggleNext(true);
     }, 200);
     if (active && over) {
       setIsDropped(true);
@@ -35,6 +37,7 @@ const DumplingGivingUI = () => {
   const handleDragStart = (event: DragStartEvent) => {
     setIsDragging(true);
     toggleBack(false); // disable going back when dragging so swipe actions don't trigger navigation
+    toggleNext(false);
   };
 
   const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
@@ -48,11 +51,18 @@ const DumplingGivingUI = () => {
     >
       <DumplingHeart dropped={isDropped} />
       <DumplingDrop dragging={isDragging} />
-      <img
-        src={pointerImage}
+
+      <motion.div
+        animate={{
+          y: [0, -10, 0],
+          transition: { repeat: Infinity },
+        }}
         className={`${(isDragging || isDropped) && "opacity-0"}
           absolute w-1/12 h-auto scale-105 -translate-x-[50%] -translate-y-[50%] top-[89%] left-[62%] rotate-270 z-30 animate-bounce transition-opacity pointer-events-none`}
-      ></img>
+      >
+        <img src={pointerImage}></img>
+      </motion.div>
+
       <span
         className={`transition-opacity delay-100 duration-300 ${isDropped ? "opacity-100" : "opacity-0"}`}
       >
