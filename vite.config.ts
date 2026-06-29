06a0b6d481550f051ac50631003cf55355bc09e4
Jessica Tong <jessicatong14@gmail.com>
2026-06-28 18:50:52 -0700

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  // base: "/afd-story"
-  
+  base: '/afd-story/',
+  plugins: [react(), tailwindcss()],
 })
